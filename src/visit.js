9ab1b04bd5a7bc9c469ec6541ad595538c77ad6f
Larry Xue <larry.p.xue@gmail.com>
2024-10-31 23:30:27 -0400

    formatting

--- src/visit.js
+++ src/visit.js
@@ -1,35 +1,32 @@
-const puppeteer = require('puppeteer')
-const config = require('./config')
-const server = require('./server')
+const puppeteer = require("puppeteer");
+const config = require("./config");
+const server = require("./server");
 
-const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
 
-const args = ['--js-flags=--jitless']
-if (server.runtime === 'aws') {
-  args.push('--no-sandbox')
+const args = ["--js-flags=--jitless"];
+if (server.runtime === "aws") {
+	args.push("--no-sandbox");
 }
 
 const browser = puppeteer.launch({
-  pipe: true,
-  dumpio: true,
-  args,
-})
+	pipe: true,
+	dumpio: true,
+	args,
+});
 
 server.run({ subscribe: true }, async ({ message }) => {
-  const { challengeId, url } = message
-  const challenge = config.challenges.get(challengeId)
+	const { challengeId, url } = message;
+	const challenge = config.challenges.get(challengeId);
 
-  let ctx
-  try {
-    ctx = await (await browser).createIncognitoBrowserContext()
-    await Promise.race([
-      challenge.handler(url, ctx),
-      sleep(challenge.timeout),
-    ])
-  } catch (e) {
-    console.error(e)
-  }
-  try {
-    await ctx.close()
-  } catch {}
-})
+	let ctx;
+	try {
+		ctx = await (await browser).createIncognitoBrowserContext();
+		await Promise.race([challenge.handler(url, ctx), sleep(challenge.timeout)]);
+	} catch (e) {
+		console.error(e);
+	}
+	try {
+		await ctx.close();
+	} catch {}
+});
